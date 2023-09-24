3ce32eb0352820cf7af6320e45edf069ef3039ed
Çağan Bıçakçı <caganbicakci97@gmail.com>
2023-09-24 22:13:14 +0300

    map module configured

---
+++ FourSquareClone/PlaceDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PlaceDetailViewController.swift
+//  FourSquareClone
+//
+//  Created by Çağan Bıçakçı on 24.09.2023.
+//
+
+import UIKit
+
+class PlaceDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FourSquareClone/PlaceModel.swift
@@ -0,0 +1,8 @@
+//
+//  PlaceModel.swift
+//  FourSquareClone
+//
+//  Created by Çağan Bıçakçı on 24.09.2023.
+//
+
+import Foundation
